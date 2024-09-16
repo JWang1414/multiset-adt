b022f9db61dfd2d7e3c9eb086342cd8fff91d66f
Jack Wang <jaack.wang@mail.utoronto.ca>
2024-09-16 12:39:30 -0400

    Created MultiSet.java abstract class

--- src/MultiSet.java
+++ src/MultiSet.java
@@ -10,5 +10,13 @@ public abstract class MultiSet {
      */
     abstract void add(Integer item);
 
-    // TODO finish adding abstract methods to fully describe what it means to be a MultiSet.
+    abstract void remove(Integer item);
+
+    abstract void contains(Integer item);
+
+    abstract boolean is_empty();
+
+    abstract int count(Integer item);
+
+    abstract void size();
 }
